4a60fade4d0daf0dd155bd31193cec689d54a6e8
PiotrOzga <149680125+PiotrOzga@users.noreply.github.com>
2023-11-17 10:14:26 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,46 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Management;
+using System.Text;
 
 namespace Win32_Product {
   class Program {
+    private static readonly string _productName = "{0} {1}";
     static void Main(string[] args) {
-      int i = 0;
-      ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_Product");
-      foreach (ManagementObject mo in mos.Get()) {
-        Console.WriteLine(mo["Name"] + " " + mo["Version"]);
-        i++;
+      if (args.Length == 0) {
+        Console.WriteLine("Invalid args");
+        return;
       }
-      Console.WriteLine(string.Format("Installed software count: {0}", i));
-      Console.ReadKey();
+      var command = args[0];
+
+      switch (command) {
+        case "screen":
+          GetProducts(command);
+          break;
+        case "file":
+          GetProducts(command);
+          break;
+        default:
+          Console.WriteLine("Invalid arg.");
+          break;
+      }
+    }
+
+    private static void GetProducts(string arg) {
+      StreamWriter StrWrriter = new StreamWriter(string.Format("Win32_Products_{0}.txt", DateTime.Now.ToString("yyyyMMdd")), false, Encoding.UTF8);
+      ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_Product");
+      Console.WriteLine("Start - please wait...");
+      foreach (ManagementObject mo in mos.Get()) {
+        switch (arg) {
+          case "screen":
+            Console.WriteLine(string.Format(_productName, mo["Name"], mo["Version"]));
+            break;
+          case "file":
+            StrWrriter.WriteLine(string.Format(_productName, mo["Name"], mo["Version"]));
+            break;
+        }
+      }
+      Console.WriteLine("Done");
     }
   }
 }
