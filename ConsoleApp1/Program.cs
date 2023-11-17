5407c5cf4ada59bd91bb39ba90c470578297b1cf
11404455 <11404455@D002WCZC7058XL3>
2023-11-17 09:35:47 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Management;
+
+namespace Win32_Product {
+  class Program {
+    static void Main(string[] args) {
+      int i = 0;
+      ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_Product");
+      foreach (ManagementObject mo in mos.Get()) {
+        Console.WriteLine(mo["Name"] + " " + mo["Version"]);
+        i++;
+      }
+      Console.WriteLine(string.Format("Installed software count: {0}", i));
+      Console.ReadKey();
+    }
+  }
+}
